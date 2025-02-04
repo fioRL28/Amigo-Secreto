71e36ecaa93ccd9ecb18c13292c3a28c37e9049d
Fiore <fiorerruiz28@gmail.com>
2025-02-04 14:46:11 -0500

    Implementación de función actualizarListaAmigos que permitirirá ver los nombres ingresados en la parte inferior

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 /*
-- Agregar nombres: Los usuarios escribirán el nombre de un amigo en un campo de texto y lo 
-agregarán a una lista visible al hacer clic en "Adicionar".
-- Validar entrada: Si el campo de texto está vacío, el programa mostrará una alerta pidiendo un nombre válido.
+
+
 - Visualizar la lista: Los nombres ingresados aparecerán en una lista debajo del campo de entrada.
 - Sorteo aleatorio: Al hacer clic en el botón "Sortear Amigo", se seleccionará aleatoriamente un nombre de la lista 
 y se mostrará en la página. */
@@ -18,8 +17,22 @@ function agregarAmigo(){
     else{
         nombreAmigos.push(nombreAmigo);
         document.getElementById('amigo').value = '';
-        
+
         console.log (nombreAmigos);
+
+        actualizarListaAmigos();
+    }
+}
+
+function actualizarListaAmigos() {
+    let lista=document.getElementById('listaAmigos');
+    
+    lista.innerHTML = '';
+
+    for(let amigo of nombreAmigos){
+        let li=document.createElement('li');
+        li.textContent=amigo;
+        lista.appendChild(li);
     }
 }
 
